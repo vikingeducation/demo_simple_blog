d89e163998fdca4de8b6dd54721bb9fc008dd7c3
ERIK TRAUTMAN <erik.trautman@gmail.com>
2014-06-17 12:23:18 -0700

    set up back end for create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,22 @@ class PostsController < ApplicationController
     @posts = Post.order(:created_at => :desc)
   end
 
+  def create
+    @post = Post.create(post_params)
+    if @post
+      redirect_to :root
+    else
+      render :index
+    end
+  end
+
+
+
+  private
+
+  # Whitelist those params!
+  def post_params
+    params.require(:post).permit(:author, :content)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+
+  validates :author, :length => { maximum: 20 }
+  validates :content, :length => { maximum: 400 } 
+
 end
