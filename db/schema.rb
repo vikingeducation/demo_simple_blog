0214c95b510ca39a4168f665ff2597ba0e2eabad
ERIK TRAUTMAN <erik.trautman@gmail.com>
2014-06-17 19:53:36 -0700

    rollback demo branch migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617221645) do
-
-  create_table "comments", force: true do |t|
-    t.string   "author"
-    t.text     "content"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140617180541) do
 
   create_table "posts", force: true do |t|
     t.string   "author"
