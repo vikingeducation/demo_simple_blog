2463a72ba502f6d7f1a84d92f560001182d0623e
ERIK TRAUTMAN <erik.trautman@gmail.com>
2014-06-17 19:40:06 -0700

    tweaks

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Post.destroy_all
+Comment.destroy_all
 
 authors = ["Joe Smith", "Jane Clark", "Sarah Brown"]
 lorem = "lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+mini_lorem = "lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 
-5.times { Post.create!(
+5.times do 
+  p = Post.create!(
             :author => authors.sample, 
             :content => lorem,
-            :created_at => rand(4).days.ago) }
+            :created_at => rand(4).days.ago)
+  rand(3).times do 
+    p.comments.create(
+            :author => authors.sample,
+            :content => mini_lorem
+            )
+  end
+end
+
